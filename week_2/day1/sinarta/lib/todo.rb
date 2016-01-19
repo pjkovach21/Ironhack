3289e74bbdf45c038a1feb43cd469576a306ad0c
Peter Kovach <pjkovach21@gmail.com>
2016-01-18 19:46:59 -0500

    find id

--- week_2/day1/sinarta/lib/todo.rb
+++ week_2/day1/sinarta/lib/todo.rb
@@ -6,7 +6,19 @@ class Todo
     def add_task(task)
     	@tasks.push(task)
     end
-    def delete(task)
-    	@tasks.delete(task)
+    def delete(task_id)
+    	@tasks = @tasks.delete_if do |task|
+    		task.id == task_id
+    	end
+    	@tasks
+    end
+    def find_id(task_id)
+    	required_task = nil
+    	@tasks.each do |task|
+    		if task.id == task_id
+    			required_task = task
+    		end
+    		required_task
+    	end
     end
 end
--- week_2/day1/sinarta/spec/todo_spec.rb
+++ week_2/day1/sinarta/spec/todo_spec.rb
@@ -1,16 +1,18 @@
 require_relative("../lib/todo.rb")
+require_relative("../lib/task.rb")
 
 RSpec.describe Todo do 
 	before:each do
 		@todo = Todo.new 
-		@task1 = "buy dog"
+		@task1 = Task.new("buy dog")
 		end
 		it "Adds task" do
 			@todo.add_task(@task1)
 		expect(@todo.tasks.size).to eq(1)
 	end
 		it "deletes tasks" do
-		@todo.delete(@tasks1)
+		@todo.add_task(@task1)
+		@todo.delete(@task1.id)
 		expect(@todo.tasks.size).to eq(0)
 	end
 end
