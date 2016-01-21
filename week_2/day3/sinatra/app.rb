aaa229efd53a152fd37238f768bdbd8acd9b888f
Peter Kovach <pjkovach21@gmail.com>
2016-01-20 21:06:27 -0500

    sinatra day1?

--- week_2/day3/sinatra/app.rb
+++ week_2/day3/sinatra/app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require
 # We're going to need to require our class files
 require_relative('lib/Task.rb')
 require_relative('lib/TodoList.rb')
+
+todo_list = TodoList.new("Josh")
+todo_list.load_tasks
+
+get "/tasks" do
+	erb(:tasks)
+end
