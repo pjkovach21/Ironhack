fe6d9fbafbadb9d52f07514d5df5d6d1b91f4a90
Peter Kovach <pjkovach21@gmail.com>
2016-01-21 19:58:48 -0500

    added buttons, try to sort by categort

--- week_2/sin_blog/app.rb
+++ week_2/sin_blog/app.rb
@@ -6,9 +6,9 @@ require "sinatra"
 require "sinatra/reloader" if development?
 
 blog = Blog.new
-blog.add_post Post.new("Pete 1" , Time.new(2014,12,1), "content")
-blog.add_post Post.new("title 2" , Time.new(2012,12,1), "coent")
-blog.add_post Post.new("ti 3" , Time.new(2013,12,1), "tent")
+blog.add_post Post.new("Pete 1" , Time.new(2014,12,1), "content" , "pete" , "love")
+blog.add_post Post.new("title 2" , Time.new(2012,12,1), "coent" , "ama" , "style")
+blog.add_post Post.new("ti 3" , Time.new(2013,12,1), "tent", "herp" , "bad things")
 
 
 get "/" do
@@ -33,7 +33,11 @@ end
 #can i get time from just clicking submit
 
 post "/writepost" do
-	@post1 = Post.new(params[:title],Time.now,params[:content])
+	@post1 = Post.new(params[:title],Time.now,params[:content],params[:author], params[:category])
 	blog.add_post(@post1)
 	redirect to "/"
+end
+
+post "/post_details/:id" do
+	redirect to("/")
 end
--- week_2/sin_blog/lib/Post.rb
+++ week_2/sin_blog/lib/Post.rb
@@ -1,8 +1,10 @@
 class Post
-	attr_accessor :title, :date, :text
-	def initialize (title, date , text)
+	attr_accessor :title, :date, :text , :author , :category
+	def initialize (title, date , text, author, category)
 		@title = title
 		@date = date
 		@text = text
+		@author = author
+		@category = category
 	end 
 end
