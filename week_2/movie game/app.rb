d9f9536eeaead4eaf66980a79c8fd803f981814e
Peter Kovach <pjkovach21@gmail.com>
2016-01-25 09:18:14 -0500

    friday project last worked on

--- week_2/movie game/app.rb
+++ week_2/movie game/app.rb
@@ -6,21 +6,41 @@ get "/" do
 	erb(:home)
 end
 
-post "/movie" do
+# post "/movie" do
+# 	@movie = params[:search]
+# 	redirect to("/imbd/#{@movie}")
+# end
+
+post "/movies" do
 	@movie = params[:search]
-	redirect to("/imbd/#{@movie}")
-end
-
-get "/imbd/:movie" do
 	
-	search = Imdb::Search.new(params[:movie])	
+	search = Imdb::Search.new(@movie)	
 
-	@number_of_results = search.movies.length
-	@movies_with_pic = search.movies[0..25]
- 	@movies_with_pic.delete_if do |film|
+	@movies_with_pic = search.movies.take(25)
+
+ 	@movies_with_poster = @movies_with_pic.delete_if do |film|
 		film.poster == nil
 	end
-	@results = @movies_with_pic[0..8]
 
+	@results = @movies_with_poster[0..15]
+
+	@win_movie = @results.sample
+
+	@results_unique = @results.delete_if do |year|
+			year.release_date == @win_movie.release_date
+		end
+
+	@final_results = @results_unique[0..7]	
+	@final_results.insert(Random.new.rand(8), @win_movie)
+	
 	erb(:imdb_test)
+end
+
+
+post "/correctmovie" do
+	"good job"
+end
+
+post "/wrongmovie" do
+	"dsakjdbksaj"
 end
