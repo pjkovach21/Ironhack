b8d907b734dc5ebbb9ec5dcd3e8062d56d073ffd
Peter Kovach <pjkovach21@gmail.com>
2016-01-28 15:52:28 -0500

    it 2 GoT

--- week3/day4/got/app.js
+++ week3/day4/got/app.js
@@ -9,9 +9,19 @@ var episodes = JSON.parse(file);
   
   function episodePrinter (episode_array) {
   for (var i = 0; i < episode_array.length ; i++) {
+        
           console.log("Title: " + episode_array[i].title + " Number: " + episode_array[i].episode_number );
           console.log(episode_array[i].description);
-          console.log("Rating: " + episode_array[i].rating)
+          console.log("Rating: " + episode_array[i].rating + starAdd(episode_array[i].rating))
 }
+}
+
+
+function starAdd (number) {
+   var stars = " "
+   for (var rate = 0; rate < number; rate++) {
+       stars += "*"
+    }
+    return stars
 }
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
