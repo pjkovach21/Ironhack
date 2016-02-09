4d909075de460af2f71044b97aafcd592cacdd49
Peter Kovach <pjkovach21@gmail.com>
2016-02-09 18:06:35 -0500

    spotify it 1- fix image problem

---
+++ w5/spotify/app.js
@@ -0,0 +1,45 @@
+
+$(document).on("ready", function (){
+	$(".js-search-spotify").on("click",function (event){
+		event.preventDefault();
+		fetchArtist();
+	});
+});
+
+function fetchArtist(){
+var seacrhArtist = $(".js-artist-spotify").val();
+console.log(seacrhArtist)
+$.ajax ({
+    url: `https://api.spotify.com/v1/search?type=artist&query=${seacrhArtist}`,
+  	
+    success: function (response) {
+      console.log(response);
+      alert("That artist is da bombbb.com");
+      displaySong(response.artists.items)
+    },
+    
+    error: function () {
+      alert("That artist is having trouble");
+    }
+  });
+}
+	
+function displaySong (artist){
+	var art_with_image = []
+	artist.forEach(artimage)
+		if artimage.images.legth > 0 
+			artimage.push
+	artist.forEach(function (art){
+		console.log(art)
+		var html = `
+			<li>
+				<h2> ${art.name} </h2>
+				<p> ${art.genres} <p>
+				<p> ${art.popularity} <p>
+				<p> ${art.followers.total} <p>
+				<p> ${art.images[0].url} <p>
+			</li>
+		`
+		$(".js-artist-list").append(html);
+	});
+}
