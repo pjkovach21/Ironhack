be0418915191771349ec6d992ac62e4d3a65a4a8
Peter Kovach <pjkovach21@gmail.com>
2016-02-13 19:25:30 -0500

    added modal to artistinfo

--- w5/spotifyPlayerSkeleton/app.js
+++ w5/spotifyPlayerSkeleton/app.js
@@ -9,7 +9,15 @@ $(document).on("ready", function (){
 		event.preventDefault();
 		playSong();
 	});
-	$('.js-player').on('timeupdate', printTime);
+	$('.js-player').on('timeupdate', printTime)
+	
+
+	$(".js-author").on("click", function(event){
+		var id = $(this).data("id")
+		event.preventDefault();
+		showArtistinfo(id);
+		$(".js-modal").modal("show");
+	});
 });
 
 function fetchSong(){
@@ -20,11 +28,13 @@ function fetchSong(){
 		success: function (response) {
 			console.log(response)
 			var songInfo = response.tracks.items[0]
+			var artistId = songInfo.artists[0].id
 			alert("I put an alert here because i can")
 			$(".js-author").text(songInfo.artists[0].name);
 			$(".js-title").text(songInfo.name);
 			$(".js-cover").prop("src",songInfo.album.images[0].url)
 			$(".js-audio").prop("src",songInfo.preview_url)
+			$(".js-author").data("id",artistId)
 		},
 		error: function (){
 			alert("shit, it aint workin")
@@ -47,17 +57,38 @@ function playSong(){
 function printTime () {
   var current = $('.js-player').prop('currentTime');
   $(".js-seekbar").val(current);
+
   console.debug('Current time: ' + current);
 }
 
-// function displaySonginfo(song) {
-// 	song.forEach (function(s){
-// 	var html = `
-// 			<li>
-// 			<h3> s.name </h3>
-// 			<p> s.artist </p>
-// 			</li>
-// 			`
-// 		$(".js-track-info").append(html)
-// 	});
-// }
+// trying to add an artist
+
+function showArtistinfo(artistId) {
+	console.log("Look at me -----", artistId)
+	// artistid.forEach (function(id){
+	$(".js-artist-name").empty();
+	$(".js-artist-img").empty();
+	$(".js-artist-followers").empty();
+	$(".js-artist-popularity").empty();
+
+	$.ajax ({
+
+	url:"https://api.spotify.com/v1/artists/" + artistId,
+	success: function (response) {
+		console.log("I am here-----", response)
+		var artistName = response.name;
+		var artistImg = response.images[0].url;
+		var artistFollowers = response.followers.total;
+		var artistPopularity = response.popularity;
+		console.log(artistName)
+		$(".js-artist-name").text(artistName);
+		$(".js-artist-img").prop("src", artistImg);
+		$(".js-artist-followers").text("Followers: " + artistFollowers);
+		$(".js-artist-popularity").text("Popularity: " + artistPopularity);
+	},
+	error: function (){
+	alert("shit, it aint workin")	
+
+	}
+	});
+}
