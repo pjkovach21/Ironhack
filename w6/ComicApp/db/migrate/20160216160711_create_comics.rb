d556e07f84577f3d621cd3980c0430a048a1edd3
Peter Kovach <pjkovach21@gmail.com>
2016-02-16 12:07:06 -0500

    authenticating with devise

---
+++ w6/ComicApp/db/migrate/20160216160711_create_comics.rb
@@ -0,0 +1,11 @@
+class CreateComics < ActiveRecord::Migration
+  def change
+    create_table :comics do |t|
+      t.string :title
+      t.string :tit
+      t.string :publisher
+
+      t.timestamps null: false
+    end
+  end
+end
