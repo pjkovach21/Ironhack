d4f28248d2a512df2e2dbfb1db58d0b81a3b472b
Peter Kovach <pjkovach21@gmail.com>
2016-02-19 10:00:19 -0500

    updated concerts

--- w4/projects/concerts/db/schema.rb
+++ w4/projects/concerts/db/schema.rb
@@ -13,8 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20160216171238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
-    t.integer  "concert_id"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,7 +49,7 @@ ActiveRecord::Schema.define(version: 20160216171238) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
